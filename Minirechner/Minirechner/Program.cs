dce6157a3bb05f99904724e8bd94949640c39389
hannesthaler <02thahan@rgtfo-me.it>
2020-04-23 11:09:23 +0200

    Addition

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -9,7 +9,26 @@ namespace Minirechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Geben sie Zahl1 ein: ");
+           
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben sie Zahl2 ein: ");
+       
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Addition: "+ Addiere(Zahl1,Zahl2)); 
+
+            Console.ReadKey();
 
         }
+
+        static int Addiere(int Zahl1, int Zahl2)
+        {
+           
+            return (Zahl1 + Zahl2);
+        }
+
+
     }
 }
